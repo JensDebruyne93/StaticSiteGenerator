55c7bf785124f6a2b7699f54feae83f25307c39a
JensDebruyne93 <jens.debruyne@live.be>
2025-06-11 00:03:52 +0200

    added image and link splitters

--- src/Inline_markdown.py
+++ src/Inline_markdown.py
@@ -22,3 +22,46 @@ def extract_markdown_images(text):
 def extract_markdown_links(text):
     matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+            if node.text_type != TextType.NORMAL_TEXT:
+                new_nodes.append(node)
+            else:
+                image_markdown = extract_markdown_images(node.text)
+                if image_markdown != None and len(image_markdown) > 0:
+                    split_nodes = node.text.split(f"![{image_markdown[0][0]}]({image_markdown[0][1]})")
+                    if len(split_nodes) == 1:
+                        new_nodes.append(TextNode(image_markdown[0][0],TextType.IMAGE, image_markdown[0][1]))
+                    else:
+                        if len(split_nodes[0]) > 0:
+                            new_nodes.append(TextNode(split_nodes[0],TextType.NORMAL_TEXT))
+                        new_nodes.append(TextNode(image_markdown[0][0],TextType.IMAGE, image_markdown[0][1]))
+                        if len(split_nodes[1]) > 0:
+                            new_nodes.extend(split_nodes_image([TextNode(split_nodes[1],TextType.NORMAL_TEXT)]))
+                else:
+                    new_nodes.append(node)
+    return new_nodes
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+            if node.text_type != TextType.NORMAL_TEXT:
+                new_nodes.append(node)
+            else:
+                link_markdown = extract_markdown_links(node.text)
+                if link_markdown != None and len(link_markdown) > 0:
+                    split_nodes = node.text.split(f"[{link_markdown[0][0]}]({link_markdown[0][1]})")
+                    if len(split_nodes) == 1:
+                        new_nodes.append(TextNode(link_markdown[0][0],TextType.LINK, link_markdown[0][1]))
+                    else:
+                        if len(split_nodes[0]) > 0:
+                            new_nodes.append(TextNode(split_nodes[0],TextType.NORMAL_TEXT))
+                        new_nodes.append(TextNode(link_markdown[0][0],TextType.LINK, link_markdown[0][1]))
+                        if len(split_nodes[1]) > 0:
+                            new_nodes.extend(split_nodes_link([TextNode(split_nodes[1],TextType.NORMAL_TEXT)]))
+                else:
+                    new_nodes.append(node)
+    return new_nodes
