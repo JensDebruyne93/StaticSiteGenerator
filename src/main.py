c58ec806bf3e633fee407bfaefc488e7f82e09d4
JensDebruyne93 <jens.debruyne@live.be>
2025-06-25 02:46:50 +0200

    add github compatibility

--- src/main.py
+++ src/main.py
@@ -3,10 +3,14 @@ from shutil import *
 from markdown_blocks import *
 from htmlnode import *
 import os
+import sys
 def main():
-   print(extract_title("# Title"))
+   if len(sys.argv) > 1:
+      basepath = sys.argv[1]
+   else:
+      basepath = "/"
    copy_to_dir("static","public")
-   generate_pages_recursive("content", "template.html", "public")
+   generate_pages_recursive("content", "template.html", "docs", basepath)
 
 def copy_to_dir(source, destination):
    clear_directory(destination)
@@ -22,7 +26,6 @@ def copy_items(source, items, destination):
          copy(f"{source}/{item}",destination)
 
 def clear_directory(destination):
-   print(os.path.isdir(destination))
    if os.path.isdir(destination) == True:
       rmtree(destination)
    os.mkdir(destination)
@@ -35,35 +38,32 @@ def extract_title(markdown):
          return line[2:]
    raise Error("No title found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-   print(os.path.isfile(from_path))
    with open(from_path, "rt") as f:
       markdown = f.read()
    with open(template_path, "rt") as f:
       template = f.read()
    HTMLnode = markdown_to_html_node(markdown)
    HTML_string = HTMLnode.to_html()
-   print(HTMLnode)
-   print(HTML_string)
    title = extract_title(markdown)
-   print(template)
    template = template.replace("{{ Title }}",title)
    template = template.replace("{{ Content }}", HTML_string)
-   print(template)
+   template = template.replace('href="/',f'href="{basepath}')
+   template = template.replace('src="/',f'src="{basepath}')
    with open(dest_path, "w") as f:
       f.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
    content = os.listdir(dir_path_content)
    for item in content:
       if os.path.isfile(os.path.join(dir_path_content, item)):
          if item.endswith(".md"):
             dest_name = item.replace(".md", ".html")
-            generate_page(os.path.join(dir_path_content,item),template_path, os.path.join(dest_dir_path, dest_name))
+            generate_page(os.path.join(dir_path_content,item),template_path, os.path.join(dest_dir_path, dest_name), basepath)
       if os.path.isdir(os.path.join(dir_path_content, item)):
          os.makedirs(os.path.join(dest_dir_path, item), exist_ok=True)
-         generate_pages_recursive(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, item))
+         generate_pages_recursive(os.path.join(dir_path_content, item), template_path, os.path.join(dest_dir_path, item), basepath)
 
 if __name__ == "__main__":
     main()
